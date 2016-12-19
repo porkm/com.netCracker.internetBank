fd3641364917434bded40909acdaf4b84cd221c9
porkm <porkm@mail.ru>
2016-12-19 15:25:17 +0300

    add MainController

---
+++ src/main/controllers/MainController.java
@@ -0,0 +1,34 @@
+package main.controllers;
+
+import main.dal.entinties.Customer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class MainController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("userJSP", new Customer());
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/check-user")
+    public ModelAndView checkUser(@ModelAttribute("userJSP") Customer user) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("secondPage");
+        modelAndView.addObject("userJSP", user);
+
+        return modelAndView;
+    }
+}
+
